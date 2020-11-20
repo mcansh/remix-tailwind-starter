359b4da4de85616e05209967e0a880fc33ad444e
Logan McAnsh <logan@mcan.sh>
2020-11-20 12:29:47 -0500

    fix: dev server
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- start.js
+++ start.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
     pretty: true,
     transpileOnly: true,
     ignore: ['/node_modules/, /__tests__/'],
-    project: require.resolve('./tsconfig.json'),
+    project: require.resolve('./server/tsconfig.json'),
   });
   require('./server');
 }
