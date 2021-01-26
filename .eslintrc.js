7d34f6e30783d9fd6edb93782ef8574ff3eda94a
Logan McAnsh <logan@mcan.sh>
2021-01-26 16:48:18 -0500

    chore: more updates for remix 0.9
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,6 @@ const path = require('path');
 module.exports = {
   extends: ['@mcansh/eslint-config/typescript'],
   parserOptions: {
-    project: [
-      path.join(process.cwd(), 'config/shared-tsconfig.json'),
-      path.join(process.cwd(), 'app/tsconfig.json'),
-      path.join(process.cwd(), 'data/tsconfig.json'),
-    ],
+    project: [path.join(process.cwd(), 'app/tsconfig.json')],
   },
 };
