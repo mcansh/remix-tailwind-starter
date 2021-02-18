897e27e63236d148e5c528be443b58991ee3e178
Logan McAnsh <logan@mcan.sh>
2021-02-17 19:11:46 -0500

    chore(deps): bump to latest (#6)
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pm2.config.js
+++ pm2.config.js
@@ -16,13 +16,5 @@ module.exports = {
         NODE_ENV: 'development',
       },
     },
-    {
-      name: 'Styles',
-      script: 'postcss styles --base styles --dir app/ -w',
-      ignore_watch: ['.'],
-      env: {
-        NODE_ENV: 'development',
-      },
-    },
   ],
 };
