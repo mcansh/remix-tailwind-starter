007fce79e7134fd014cb963b5293b133410ce1d8
Logan McAnsh <logan@mcan.sh>
2021-03-31 22:51:01 -0400

    feat: deploy to fly.io 🛫
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const { createRequestHandler } = require('@remix-run/vercel');
+const express = require('express');
+const { createRequestHandler } = require('@remix-run/express');
 
-module.exports = createRequestHandler({
-  build: require('./build'),
+const app = express();
+
+app.use(express.static('public'));
+
+app.all(
+  '*',
+  createRequestHandler({
+    build: require('./build'),
+  })
+);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express server started on http://localhost:${port}`);
 });
