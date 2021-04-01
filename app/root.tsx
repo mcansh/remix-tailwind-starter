b804acb53e820eee7723102e7d1d178d9e9ff730
Logan McAnsh <logan@mcan.sh>
2021-04-01 01:32:19 -0400

    feat: add ErrorBoundary
    (cherry picked from commit 223656650435640f6b44207db61fc84fd0693247)

--- app/root.tsx
+++ app/root.tsx
@@ -52,5 +52,33 @@ const App: React.VFC = () => {
   );
 };
 
+interface ErrorBoundaryProps {
+  error: Error
+}
+
+const ErrorBoundary: React.VFC<ErrorBoundaryProps> = ({ error }) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Oops!</title>
+        <Links />
+      </head>
+      <body>
+        <div>
+          <h1>App Error</h1>
+          <pre>{error.message}</pre>
+          <p>
+            Replace this UI with what you want users to see when your app throws
+            uncaught errors. The file is at <code>app/App.tsx</code>.
+          </p>
+        </div>
+
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
 export default App;
-export { links, loader, meta };
+export { ErrorBoundary, links, loader, meta };
