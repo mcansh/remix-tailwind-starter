10649e2e0f4e5243b94e3b388dafd75dbfe37386
Logan McAnsh <logan@mcan.sh>
2021-04-08 00:20:12 -0400

    chore: remove redirect
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const app = express();
 
 app.use(express.static('public', { immutable: true, maxAge: '1y' }));
 
-// it doesn't appear fly.io will auto upgrade connections
-app.use((req, res, next) => {
-  if (req.get('X-Forwarded-Proto') === 'http') {
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  }
-  return next();
-});
-
 if (process.env.NODE_ENV !== 'production') {
   const cwd = process.cwd();
   app.all('*', (req, res) => {
