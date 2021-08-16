ce40876429cc0ff5f1a3d83b88184281151423c5
Logan McAnsh <logan@mcan.sh>
2021-08-16 12:18:30 -0400

    chore: update pm2 config
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pm2.config.js
+++ pm2.config.js
@@ -1,22 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'Express',
-      script: 'node index.js',
-      watch: ['index.js'],
-      watch_options: { followSymlinks: false },
-      env: {
-        NODE_ENV: 'development',
-      },
+      name: 'Remix',
+      script: 'remix run',
+      ignore_watch: ['.'],
     },
     {
       name: 'CSS',
       script: 'postcss styles --base styles --dir app/styles --ext css -w',
-      ignore_watch: ['.'],
-    },
-    {
-      name: 'Remix',
-      script: 'remix run2',
       ignore_watch: ['.'],
     },
   ],
